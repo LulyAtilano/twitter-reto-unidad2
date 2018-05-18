6ab498e4a9b80d73d882288c989eab40c6fc0317
latilanoh@gmail.com <latilanoh@gmail.com>
2018-05-17 23:26:00 -0500

    Se trabaja con la funcionalidad para cumplir el 2do punto de la version dos, correspondiente a que aparezca el conteo de caracteres en forma regresiva

--- js/app.js
+++ js/app.js
@@ -9,10 +9,30 @@ enterTweet.addEventListener('click', function() {
     var container = document.getElementById('containerTweet');
 
     var saveTweet = document.createElement('p');
+    saveTweet.id = "newTweet";
     var insertTweet = document.createTextNode(firstTweet);
+    
+    // Se crea el número que dirá la cantidad de caracteres restantes
+    var number = document.createElement('span');
+    var printNumber = document.createTextNode(number);
+    var sectionTweet = document.getElementById('textTweet');
 
     // Anidar elementos para que aparezca el tweet en la sección Container Tweet
+    sectionTweet.appendChild(printNumber);
     saveTweet.appendChild(insertTweet);
     container.appendChild(saveTweet);
+
+    // Versión 2 -> Si no hay texto deshabilitar el gotón de tweet
+    if (insertTweet == null ) {
+        enterTweet.disabled = true; 
+    }
+        enterTweet.disabled = false; 
+    
+    var textOfTweet = document.getElementById('newTweet').firstChild;
+    
+    for ( var i = 140 ; i < textOfTweet.length; i--) {
+        console.log(i);
+        console.log(textOfTweet);
+    }
 });
 
